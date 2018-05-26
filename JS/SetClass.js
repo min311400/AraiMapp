3b16e6570773ab8ed680a97d9ff2e0877d80db41
min311400 <33981942+min311400@users.noreply.github.com>
2018-05-26 22:46:56 +0700

    Add files via upload

--- JS/SetClass.js
+++ JS/SetClass.js
@@ -4,72 +4,84 @@ $(document).ready(function(){
         $(".Content2").removeClass("w3-hide");
         $(".Content1").addClass("w3-hide");
         $(".Content3").addClass("w3-hide");
+        $(".ContentP").addClass("w3-hide");
     });
     
     $(".Content31").click(function(){
         $(".Content4").removeClass("w3-hide");
         $(".Content1").addClass("w3-hide");
         $(".Content3").addClass("w3-hide");
+        $(".ContentP").addClass("w3-hide");
     });
 
     $(".Content51").click(function(){
         $(".Content6").removeClass("w3-hide");
         $(".Content5").addClass("w3-hide");
         $(".Content7").addClass("w3-hide");
+        $(".ContentP").addClass("w3-hide");
     });
     
     $(".Content71").click(function(){
         $(".Content8").removeClass("w3-hide");
         $(".Content5").addClass("w3-hide");
         $(".Content7").addClass("w3-hide");
+        $(".ContentP").addClass("w3-hide");
     });
 
     $(".Content91").click(function(){
         $(".Content10").removeClass("w3-hide");
         $(".Content9").addClass("w3-hide");
         $(".Content12").addClass("w3-hide");
+        $(".ContentP").addClass("w3-hide");
     });
     
     $(".Content121").click(function(){
         $(".Content13").removeClass("w3-hide");
         $(".Content9").addClass("w3-hide");
         $(".Content12").addClass("w3-hide");
+        $(".ContentP").addClass("w3-hide");
     });
     // secound page
    $(".Content21").click(function(){
         $(".Content1").removeClass("w3-hide");
         $(".Content3").removeClass("w3-hide");
         $(".Content2").addClass("w3-hide");
+        $(".ContentP").removeClass("w3-hide");
     });
   
     $(".Content41").click(function(){
         $(".Content1").removeClass("w3-hide");
         $(".Content3").removeClass("w3-hide");
         $(".Content4").addClass("w3-hide");
+        $(".Content1").removeClass("w3-hide");
     });
 
     $(".Content61").click(function(){
         $(".Content5").removeClass("w3-hide");
         $(".Content7").removeClass("w3-hide");
         $(".Content6").addClass("w3-hide");
+        $(".Content1").removeClass("w3-hide");
     });
   
     $(".Content41").click(function(){
         $(".Content5").removeClass("w3-hide");
         $(".Content7").removeClass("w3-hide");
         $(".Content8").addClass("w3-hide");
+        $(".Content1").removeClass("w3-hide");
     });
 
     $(".Content101").click(function(){
         $(".Content9").removeClass("w3-hide");
         $(".Content12").removeClass("w3-hide");
         $(".Content10").addClass("w3-hide");
+        $(".Content1").removeClass("w3-hide");
     });
   
     $(".Content131").click(function(){
         $(".Content9").removeClass("w3-hide");
         $(".Content10").removeClass("w3-hide");
         $(".Content13").addClass("w3-hide");
+        $(".Content1").removeClass("w3-hide");
     });
 
 
